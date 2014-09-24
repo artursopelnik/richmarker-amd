c5733a080169f4055367bb6c34f2d6fe2a3a7efd
chirag <jain_chirag04@yahoo.com>
2014-09-24 15:19:23 +0530

    use https

--- src/richmarker.js
+++ src/richmarker.js
@@ -1,4 +1,4 @@
-define(['async!http://maps.googleapis.com/maps/api/js?v=3.16&sensor=false&libraries=places,geometry'], function() {
+define(['async!https://maps.googleapis.com/maps/api/js?v=3.16&sensor=false&libraries=places,geometry'], function() {
 
 // ==ClosureCompiler==
 // @compilation_level ADVANCED_OPTIMIZATIONS
